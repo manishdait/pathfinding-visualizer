449929d323769ccf7766fed4af6b0f533019eb5f
Manish Dait <daitmanish88@gmail.com>
2023-06-22 20:53:52 +0530

    Added disable create and remove wall and weight func.

--- src/app/mouse-activites.directive.ts
+++ src/app/mouse-activites.directive.ts
@@ -7,49 +7,50 @@ export class MouseActivitesDirective {
 
   @Input() isWeight: any;
   @Input() isWall: any;
+  @Input() isDisable: any;
 
   constructor(private element: ElementRef, private renderer: Renderer2) { 
     this.renderer.setStyle(this.element.nativeElement,'cursor','pointer');
   }
 
   @HostListener('mouseover',['$event'])createWall(event: MouseEvent) {
-    var ele = this.element.nativeElement.firstChild;
-    if(this.isWall && event.altKey) {
-      if(!ele.hasChildNodes()) {
-        ele.classList.add('wall');
+    if(!this.isDisable) {
+      var ele = this.element.nativeElement.firstChild;
+      if(this.isWall && event.altKey) {
+        if(!ele.hasChildNodes()) {
+          ele.classList.add('wall');
+        }
       }
     }
-
-    
   }
 
   @HostListener('click') createWeight() {
-
-    var ele = this.element.nativeElement.firstChild;
-    if(ele.classList.contains('wall')) {
-      ele.classList.remove('wall');
-      return;
-    }
-
-    if(ele.hasChildNodes()) {
-      if(ele.firstChild.id != 'start' && ele.firstChild.id != 'goal' && ele.firstChild.id != 'boom') {
-        this.element.nativeElement.setAttribute('weight','1');
-        ele.removeChild(ele.firstChild);
+    if(!this.isDisable) {
+      var ele = this.element.nativeElement.firstChild;
+      if(ele.classList.contains('wall')) {
+        ele.classList.remove('wall');
+        return;
       }
-      return;
-    }
 
-    if(this.isWeight) {
-      var img = document.createElement('span');
-      img.className = 'material-symbols-outlined';
-      img.classList.add('icon');
-      img.innerHTML = 'weight';
-      img.classList.add('fill');
-      this.element.nativeElement.setAttribute('weight','15');
-      ele.appendChild(img);
-      console.log(this.isWeight)
-    }
+      if(ele.hasChildNodes()) {
+        if(ele.firstChild.id != 'start' && ele.firstChild.id != 'goal' && ele.firstChild.id != 'boom') {
+          this.element.nativeElement.setAttribute('weight','1');
+          ele.removeChild(ele.firstChild);
+        }
+        return;
+      }
 
+      if(this.isWeight) {
+        var img = document.createElement('span');
+        img.className = 'material-symbols-outlined';
+        img.classList.add('icon');
+        img.innerHTML = 'weight';
+        img.classList.add('fill');
+        this.element.nativeElement.setAttribute('weight','15');
+        ele.appendChild(img);
+        console.log(this.isWeight)
+      }
+    }
   }
 
 }
